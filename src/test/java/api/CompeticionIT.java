0ec30df44b4c7dabc96617c8d8707fd328282ec8
Jose Lorenzo <jolorenzom@gmail.com>
2018-10-13 17:30:39 +0200

    [issue#02] Implementada petición GET /competiciones al API con test de prueba

--- src/test/java/api/CompeticionIT.java
+++ src/test/java/api/CompeticionIT.java
@@ -2,6 +2,7 @@ package api;
 
 import api.apiControllers.CompeticionApiController;
 import api.dtos.CompeticionDto;
+import api.dtos.CompeticionIdNombreDto;
 import http.Client;
 import http.HttpException;
 import http.HttpRequest;
@@ -9,9 +10,11 @@ import http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CompeticionIT {
 
@@ -44,6 +47,16 @@ public class CompeticionIT {
     private String createCompeticion() {
         HttpRequest request = HttpRequest.builder(CompeticionApiController.COMPETICIONES).body(new CompeticionDto("La Vuelta")).post();
         return (String) new Client().submit(request).getBody();
+    }
+
+    @Test
+    void testReadAll() {
+        for (int i = 0; i < 5; i++) {
+            this.createCompeticion();
+        }
+        HttpRequest request = HttpRequest.builder(CompeticionApiController.COMPETICIONES).get();
+        List<CompeticionIdNombreDto> competiciones = (List<CompeticionIdNombreDto>) new Client().submit(request).getBody();
+        assertTrue(competiciones.size() >= 5);
     }
 
 }
