493de05ba3b505c001bfb023ff2b89872433da26
Jose Lorenzo <jolorenzom@gmail.com>
2018-10-14 12:09:36 +0200

    [issue#05] Añadidos test y refactor entity Competicion

--- src/test/java/api/EquipoIT.java
+++ src/test/java/api/EquipoIT.java
@@ -6,8 +6,13 @@ import api.dtos.CompeticionDto;
 import api.dtos.EquipoDto;
 import api.entities.Categoria;
 import http.Client;
+import http.HttpException;
 import http.HttpRequest;
+import http.HttpStatus;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EquipoIT {
 
@@ -26,5 +31,30 @@ public class EquipoIT {
     private String createCompeticion() {
         HttpRequest request = HttpRequest.builder(CompeticionApiController.COMPETICIONES).body(new CompeticionDto("La Vuelta")).post();
         return (String) new Client().submit(request).getBody();
+    }
+
+
+    @Test
+    void testCreateEquipoWithoutEquipoDtoNombre() {
+        String competicionId = this.createCompeticion();
+        HttpRequest request = HttpRequest.builder(EquipoApiController.EQUIPOS)
+                .body(new EquipoDto(null, Categoria.ELITE, competicionId )).post();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
+    }
+
+    @Test
+    void testCreateEquipoCompeticionIdNotFound() {
+        HttpRequest request = HttpRequest.builder(EquipoApiController.EQUIPOS)
+                .body(new EquipoDto("Equipo Sky", Categoria.SENIOR, "9999")).post();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.NOT_FOUND, exception.getHttpStatus());
+    }
+
+    @Test
+    void testCreateEquipoWithoutCategoriaCompeticion() {
+        HttpRequest request = HttpRequest.builder(EquipoApiController.EQUIPOS)
+                .body(new EquipoDto("Equipo BMC", null, null)).post();
+        new Client().submit(request);
     }
 }
